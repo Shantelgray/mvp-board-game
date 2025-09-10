f853a14af40137f34fc264dba7456e5f84ad4eba
Shantel <Shantelgray24@gmail.com>
2025-09-10 13:19:44 -0400

    Fixing the errors on the view page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
   def total_points
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
 
   resources :games do
-  resources :scores, only: [:index, :new, :create, :show]
+    resources :scores, only: %i[index new create show]
   end
 end
